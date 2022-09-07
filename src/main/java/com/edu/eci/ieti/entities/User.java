92302efca6e55729cd8b07431744171ee45731c8
María José Torres Nieves <majito.200130@gmail.com>
2022-09-06 22:03:38 -0500

    fix

--- src/main/java/com/edu/eci/ieti/entities/User.java
+++ src/main/java/com/edu/eci/ieti/entities/User.java
@@ -17,6 +17,8 @@ public class User {
     private String lastName;
     private Date createdAt;
 
+    public User() {
+    }
     public User(String id, String name, String email, String lastName, Date createdAt) {
         this.id = id;
         this.name = name;
@@ -24,48 +26,36 @@ public class User {
         this.lastName = lastName;
         this.createdAt = createdAt;
     }
-
-    public User(){}
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public String getLastname() {
+    public String getLastName() {
         return lastName;
     }
-
-    public void setLastname(String lastName) {
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public Date getCreatedAt() {
         return createdAt;
     }
-
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
-
     public UserDto toDto(){
         return new UserDto(id, name, email, lastName, createdAt.toString());
     }
